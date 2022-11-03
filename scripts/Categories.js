7ae818d4c87dded37ee325c53bab9b0ff33770ab
Amy Ulmer <aulmer703@gmail.com>
2022-11-03 14:08:49 -0500

    Changes

--- scripts/Categories.js
+++ scripts/Categories.js
@@ -1,17 +1,40 @@
-import { getCategories } from "./database.js"
+import { getCategories } from "./database.js";
+import { getArticles } from "./database.js";
 
-const categories = getCategories()
+const categories = getCategories();
+const articles = getArticles();
 
 export const categoryHTML = () => {
-    let html = "<ul>"
+  let html = "<ul>";
 
-    const listItems = categories.map(category => {
-        return `<li id="${category.id}"> ${category.name} 
-        </li>`
-    })
+  const listItems = categories.map((category) => {
+    return `<li id="category--${category.id}"> ${category.name} 
+        </li>`;
+  });
 
-    html += listItems.join("")
-    html += "</ul>"
+  html += listItems.join("");
+  html += "</ul>";
 
-    return html
-}
+  return html;
+};
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.id.startsWith("category")) {
+    const [, categoryId] = itemClicked.id.split("--");
+    
+    let articleCount = 0;
+    for (const category of categories) {
+      if (category.id === parseInt(categoryId)) {
+        for (const article of articles) {
+        //   let articleCount = 0;
+          if (article.categoryId === category.id) {
+            articleCount++;
+
+          }
+        }
+        window.alert(`${category.name} has ${articleCount} articles`);
+      }
+    }
+  }
+});
